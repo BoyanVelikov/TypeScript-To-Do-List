40a84952962d4fcccc9370b601f7e81beffd558d
Boyan Velikov <boyanvelikov@gmail.com>
2022-10-17 09:00:47 +0300

    17-10-2022

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+// import {v4 as uuidV4} from 'uuid';
 
-import confetti from 'canvas-confetti';
+// console.log(uuidV4());
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+const list = document.querySelector('#list');
+const form = document.querySelector('#new-task-form');
+const input = document.querySelector('#new-task-title');
