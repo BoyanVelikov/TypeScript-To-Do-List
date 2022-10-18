a11a4ed9df6655e49cbd44f8f55eedaba580545d
Boyan Velikov <boyanvelikov@gmail.com>
2022-10-18 09:00:52 +0300

    18-10-2022

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
-// import {v4 as uuidV4} from 'uuid';
+import {v4 as uuidV4} from 'uuid';
 
-// console.log(uuidV4());
+console.log(uuidV4());
 
-const list = document.querySelector('#list');
-const form = document.querySelector('#new-task-form');
-const input = document.querySelector('#new-task-title');
+const list = document.querySelector<HTMLUListElement>('#list');
+const form = document.querySelector('new-task-form') as HTMLFormElement || null;
+const input = document.querySelector<HTMLInputElement>('#new-task-title');
+
+form?.addEventListener('submit', e => {
+
+    if (input?.value == "" || input?.value == null) return
+
+    const task = {
+        id: uuidV4(),
+        title: input.value,
+        completed: false,
+        createdAt: new Date()
+    }
+
+})
